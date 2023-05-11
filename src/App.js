ed004b5874aea41465625cc7d0a1ce294bc13b25
enigmaSpy <soczekv18@gmail.com>
2023-05-11 13:46:21 +0200

    added session login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { useHiragana } from './useHiragana';
 import MainContent from './MainContent/Form'
 import Header from './Header'
 import StartButton from './StartButton';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
   const [charData, removeChar] = useHiragana();
-  const [startQuiz, setStartQuiz] = useState(false)
+  const [startQuiz, setStartQuiz] = useState(JSON.parse(sessionStorage.getItem("sessionLogin"))||false)
+
+  useEffect(()=>{
+    sessionStorage.setItem("sessionLogin", JSON.stringify(startQuiz));
+  },[startQuiz])
 
   const handleStart = ()=>{
     setStartQuiz(true);
