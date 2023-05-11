a27a126fa207b6520418f528de8e83e55989cdac
enigmaSpy <soczekv18@gmail.com>
2023-05-11 13:31:44 +0200

    added localStorage

--- src/useHiragana.js
+++ src/useHiragana.js
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import hiraganaData from './hiragana.json';
 
 export const useHiragana = () => {
-    const [charData, setCharData] = useState(
-      hiraganaData.map((item, index) => ({
-        id: index + 1,
-        ...item,
-        know: false
-      }))
-    );
+  const [charData, setCharData] = useState(
+    JSON.parse(localStorage.getItem("charData")) ||
+    hiraganaData.map((item, index) => ({
+      id: index + 1,
+      ...item,
+      know: false
+    }))
+  );
 
-    const removeChar = (id) => {
-      setCharData(charData.filter((item) => item.id !== id));
-    };
-  
-    return [charData, removeChar];
+  useEffect(() => {
+    localStorage.setItem("charData", JSON.stringify(charData));
+  }, [charData]);
+
+  const removeChar = (id) => {
+    setCharData(charData.filter((item) => item.id !== id));
+  };
+  return [charData, removeChar];
 };
